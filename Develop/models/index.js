550922bb9257571c2a1b515337b84242925a8205
MERYEM-AD <adiri.meryem.mw@gmail.com>
2021-12-26 11:33:48 -0500

    set up relation between Model

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,37 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { Model } = require('sequelize/types');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+  through: {
+    model: ProductTag,
+  },
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
+  through: {
+    model: ProductTag,
+  },
+});
+
 
 module.exports = {
   Product,
